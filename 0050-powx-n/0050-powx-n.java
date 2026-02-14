ea58a3eec65171f3bacb605150fc0337f9ddcc6b
Aditya Suman <adityasuman14112004@gmail.com>
2026-02-14 10:49:15 +0530

    Time: 0 ms (100%), Space: 48.3 MB (33.59%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,6 +1,18 @@
 class Solution {
     public double myPow(double x, int n) {
-        double result = Math.pow(x,n);
-        return result;
+        long binary = n;
+        if(n<0){
+            x=1/x;
+            binary = -binary;
+        }
+        double ans = 1;
+        while(binary>0){
+            if(binary %2==1){
+                ans*=x;
+            }
+            x*=x;
+            binary/=2;
+        }
+        return ans;
     }
 }
