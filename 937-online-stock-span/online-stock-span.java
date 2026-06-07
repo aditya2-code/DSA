0ea200ab291dd27ea249d08737fefb922a596b5a
Aditya Suman <adityasuman14112004@gmail.com>
2026-06-07 11:14:22 +0530

    Time: 29 ms (85.71%) | Memory: 54.6 MB (29.68%) - LeetSync

--- 937-online-stock-span/online-stock-span.java
+++ 937-online-stock-span/online-stock-span.java
@@ -1,17 +1,18 @@
 class StockSpanner {
-    private Stack<int[]> stack;
+    Stack<int[]> stack;
 
     public StockSpanner() {
         stack = new Stack<>();
     }
     
     public int next(int price) {
-        int span = 1;
-        while(!stack.isEmpty() && stack.peek()[0] <= price){
+        int span =1;
+        while(!stack.isEmpty() && stack.peek()[0]<= price){
             span += stack.pop()[1];
         }
-        stack.push(new int[]{price,span});
+        stack.push(new int[]{price, span});
         return span;
+        
     }
 }
 
