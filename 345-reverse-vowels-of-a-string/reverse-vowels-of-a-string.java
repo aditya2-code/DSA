84a3c7c26b102241938cc244a8beb703e041c0fd
Aditya Suman <adityasuman14112004@gmail.com>
2026-06-23 06:40:13 +0530

    Time: 2 ms (99.29%) | Memory: 46.6 MB (65.91%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,20 +1,40 @@
 class Solution {
     public String reverseVowels(String s) {
-        Stack<Character> rev = new Stack<>();
-        for(int i = 0; i< s.length();i++){
-            if(s.charAt(i)=='a' ||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'||s.charAt(i)=='A'||s.charAt(i)=='E'||s.charAt(i)=='I'||s.charAt(i)=='O'||s.charAt(i)=='U'){
-                rev.push(s.charAt(i));
+
+        char[] chars = s.toCharArray();
+
+        int start = 0 ;
+
+        int end = s.length()-1;
+
+        while (start<end){
+            while (start < end && !isVowel(chars[start])) {
+                start++;
             }
+
+            while (start < end && !isVowel(chars[end])) {
+                end--;
+            }
+
+            if (start < end) {
+                swap(chars, start, end);
+                start++;
+                end--;
+            }
+
         }
-        String ans = "";
-        for(int i = 0; i<s.length();i++){
-            if(s.charAt(i)=='a' ||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'||s.charAt(i)=='A'||s.charAt(i)=='E'||s.charAt(i)=='I'||s.charAt(i)=='O'||s.charAt(i)=='U'){
-                ans+= rev.pop();
-            }
-            else{
-                ans+= s.charAt(i);
-            }
-        }
-        return ans;
+
+        return new String(chars);
+    }
+
+    private void swap(char[] word, int start, int end){
+        char temp = word[start];
+        word[start] = word[end];
+        word[end] = temp;
+    }
+
+    private boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+                || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
     }
 }
